c360070def656f788666c8eebf63aa4fe9a1c2fb
Sofiane <derraz.sofiane@gmail.com>
2019-08-29 15:30:03 +0200

    BDD: Migrations: Ajout Table: Users et Clients

--- Livrable/Gestionnaire_de_Projets/database/migrations/2014_10_12_000000_create_users_table.php
+++ Livrable/Gestionnaire_de_Projets/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->increments('id');
+            $table->string('Nom');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',array('ADMIN','GERANT','CHEF_PROJET','EMPLOYE'));
+            $table->longText('commentaire');
             $table->rememberToken();
             $table->timestamps();
         });
